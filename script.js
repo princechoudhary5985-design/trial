4fe4d690f250ed5b527a47e08baed8862d47df15
princechoudhary5985-design <princechoudhary5985@gmail.com>
2025-09-03 01:05:50 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+document.getElementById("kitty").addEventListener("click", () => {
+  // Show birthday message
+  document.getElementById("birthday-message").classList.remove("hidden");
+
+  // Create balloons
+  for (let i = 0; i < 20; i++) {
+    let balloon = document.createElement("div");
+    balloon.classList.add("balloon");
+
+    // Random colors
+    balloon.style.background = `hsl(${Math.random() * 360}, 70%, 60%)`;
+
+    // Random position
+    balloon.style.left = Math.random() * 100 + "vw";
+
+    // Different sizes
+    balloon.style.width = 40 + Math.random() * 40 + "px";
+    balloon.style.height = 60 + Math.random() * 40 + "px";
+
+    // Add balloon
+    document.getElementById("balloons").appendChild(balloon);
+
+    // Remove balloon after animation
+    setTimeout(() => {
+      balloon.remove();
+    }, 6000);
+  }
+});
